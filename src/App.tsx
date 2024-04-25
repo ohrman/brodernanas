2002ec0fae5b0386f7d7a81cb5ffb788f1c20bde
Jonas Mellström <jonas.mellstrom@pagen.se>
2024-04-25 16:51:49 +0200

    change to a react app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <title>Bröderna näs INC</title>
+
+      <header>
+        <h1></h1>
+        <img src="broderna.jpg" alt="Bröderna"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
